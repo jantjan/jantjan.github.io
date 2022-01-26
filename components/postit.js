85fb31ad66872283a73d2150548d1152c8fd19a5
janjtjan <tjan.janice@gmail.com>
2022-01-26 17:10:39 -0500

    new post it update

--- components/postit.js
+++ components/postit.js
@@ -9,3 +9,14 @@ checkbox.onclick = function () {
         checkbox1.src = img_1;
     }
 }
+var checkbox = document.getElementById('checkbox2');
+        var img_1 = 'https://i.imgur.com/IRvthMA.png';
+        var img_2 = 'https://i.imgur.com/QymO2Qr.gif';
+
+checkbox.onclick = function () {
+    if (checkbox2.src == img_1) {
+        checkbox2.src = img_2;
+    } else if (checkbox2.src == img_2) {
+        checkbox2.src = img_1;
+    }
+}
