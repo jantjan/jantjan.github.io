99cb9b5c3d24a4cb6c54b084a4ec11825763dc86
janjtjan <tjan.janice@gmail.com>
2022-01-26 21:39:53 -0500

    checkbox vars fixed

--- components/postit.js
+++ components/postit.js
@@ -1,24 +1,23 @@
 
 
-var checkbox = document.getElementById('checkbox1');
-        var img_1 = 'https://i.imgur.com/FT27YXo.png';
-        var img_2 = 'https://i.imgur.com/69NSeT9.gif';
-
-checkbox.onclick = function () {
-    if (checkbox1.src == img_1) {
-        checkbox1.src = img_2;
-    } else if (checkbox1.src == img_2) {
-        checkbox1.src = img_1;
+var checkbox1 = document.getElementById('checkbox1');
+    var img_1_1 = 'https://i.imgur.com/FT27YXo.png';
+    var img_1_2 = 'https://i.imgur.com/69NSeT9.gif';
+checkbox1.onclick = function () {
+    if (checkbox1.src == img_1_1) {
+        checkbox1.src = img_1_2;
+    } else if (checkbox1.src == img_1_2) {
+        checkbox1.src = img_1_1;
     }
 }
-var checkbox = document.getElementById('checkbox2');
-        var img_1 = 'https://i.imgur.com/IRvthMA.png';
-        var img_2 = 'https://i.imgur.com/QymO2Qr.gif';
 
-checkbox.onclick = function () {
-    if (checkbox2.src == img_1) {
-        checkbox2.src = img_2;
-    } else if (checkbox2.src == img_2) {
-        checkbox2.src = img_1;
+var checkbox2 = document.getElementById('checkbox2');
+        var img_2_1 = 'https://i.imgur.com/IRvthMA.png';
+        var img_2_2 = 'https://i.imgur.com/QymO2Qr.gif';
+checkbox2.onclick = function () {
+    if (checkbox2.src == img_2_1) {
+        checkbox2.src = img_2_2;
+    } else if (checkbox2.src == img_2_2) {
+        checkbox2.src = img_2_1;
     }
 }
