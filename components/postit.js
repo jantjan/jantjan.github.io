7e0d20a65775b2beb6106d382645ecde15210e1a
janjtjan <tjan.janice@gmail.com>
2022-01-26 21:12:33 -0500

    why are my post its broken again :<

--- components/postit.js
+++ components/postit.js
@@ -1,14 +1,3 @@
-var checkbox = document.getElementById('checkbox1');
-        var img_1 = 'https://i.imgur.com/FT27YXo.png';
-        var img_2 = 'https://i.imgur.com/69NSeT9.gif';
-
-checkbox.onclick = function () {
-    if (checkbox1.src == img_1) {
-        checkbox1.src = img_2;
-    } else if (checkbox1.src == img_2) {
-        checkbox1.src = img_1;
-    }
-}
 var checkbox = document.getElementById('checkbox2');
         var img_1 = 'https://i.imgur.com/IRvthMA.png';
         var img_2 = 'https://i.imgur.com/QymO2Qr.gif';
@@ -18,5 +7,17 @@ checkbox.onclick = function () {
         checkbox2.src = img_2;
     } else if (checkbox2.src == img_2) {
         checkbox2.src = img_1;
+    }
+}
+
+var checkbox = document.getElementById('checkbox1');
+        var img_1 = 'https://i.imgur.com/FT27YXo.png';
+        var img_2 = 'https://i.imgur.com/69NSeT9.gif';
+
+checkbox.onclick = function () {
+    if (checkbox1.src == img_1) {
+        checkbox1.src = img_2;
+    } else if (checkbox1.src == img_2) {
+        checkbox1.src = img_1;
     }
 }
