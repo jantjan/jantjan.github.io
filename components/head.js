e3e8b7dd2034c60c217e203b69b54eccc53e8759
janjtjan <tjan.janice@gmail.com>
2022-01-26 20:20:13 -0500

    test new head.js

---
+++ components/head.js
@@ -0,0 +1,39 @@
+class Head extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+
+  connectedCallback() {
+    this.innerHTML = `
+    <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-180288618-1"></script>
+            <script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'UA-180288618-1');
+            </script>
+        <!-- Google Tag Manager -->
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-54XJFPM');</script>
+        <!-- End Google Tag Manager -->
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="keywords" content="Janice Tjan, Design degree, MIT, Mechanical Engineering, Portfolio" />
+        <link rel="icon" href="assets/LOGO.jpeg">
+        <meta name="author" content="Janice Tjan" />
+
+        <script src="components/header.js" type="text/javascript" defer></script>
+    </head>
+    `;
+  }
+
+} 
+customElements.define('header-component', Head);
