1e27916baf5e96cb895dc8f36b9dd8351f6a158e
janjtjan <tjan.janice@gmail.com>
2022-09-17 01:47:34 -0400

    add statistical analysis paper

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,11 +1,4 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
-require_once __DIR__ . '/vendor/phpmailer/src/Exception.php';
-require_once __DIR__ . '/vendor/phpmailer/src/PHPMailer.php';
-require_once __DIR__ . '/vendor/phpmailer/src/SMTP.php';
 
 // ENTER YOUR EMAIL
 $emailTo = "tjan.janice@gmail.com";
